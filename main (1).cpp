e9f2837798a2c0c3bebeaf0f7a8a8f3709297638
Naruto Uzumaki <141573479+NarutoUzumaki04@users.noreply.github.com>
2024-05-11 10:36:33 +0530

    nothing

---
+++ main (1).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    cin >> x >> y;
+    cout<<"the value of x is: " << x << " and y is: " << y;
+
+    return 0;
+}
